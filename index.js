777b3ede57987ea707f3896d560a02d07d22ab55
hira480 <hiramannan8@gmail.com>
2022-05-15 00:06:14 +0600

    create booking collection and add new data to booking collection

--- index.js
+++ index.js
@@ -19,13 +19,30 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        /**
+        * API Naming ConvenTion
+        * app.get('/booking') // get all bookings in this collection. or get more then one or by filter.
+        * app.get('/booking/:id') // get a specific booking
+        * app.post('/booking') // add a new booking.
+        * app.patch('/booking/:id') // update a specific booking
+        * app.delete('/booking/:id') // delete a specific booking
+        * */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
         })
+
     }
     finally {
 
